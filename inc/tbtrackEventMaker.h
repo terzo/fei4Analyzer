ce5594b1261a2275579fbf0e560210cb34bab4b6
Stefano Terzo <terzo@Norby.local>
2016-04-20 12:54:17 +0200

    added blank tbtrack event maker files

---
+++ inc/tbtrackEventMaker.h
@@ -0,0 +1,57 @@
+/****************************************************************************
+** Author: Stefano Terzo
+**
+** Max-Planck-instiut fuer Physik (Werner-Heisenberg-Institut)
+** Foehringer Ring 6, D-80805 Muenchen, Germany
+**
+****************************************************************************/
+
+#ifndef __TBTRACK_EVENTMAKER_H__
+#define __TBTRACK_EVENTMAKER_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+#include <TFile.h>
+#include <TH2F.h>
+#include <TTree.h>
+#include <TROOT.h>
+#include <TParameter.h>
+
+#include <TString.h>
+
+#include "FormattedRecord.hh"
+#include "EventMaker.h"
+#include "macros.h"
+#include "ANSIColors.h" 
+
+class tbtrackEventMaker : public EventMaker
+{
+ public :
+ 
+   tbtrackEventMaker(bool quiet =  false, bool readTimeStamp = false, bool design25=false)  ;
+  ~tbtrackEventMaker(void                    ) {;}
+  
+   hitMapDef makeEvents(std::string infilename, std::string outfilename = "", int lv1diff = 1, int nevt = -1);
+   void setQuiet(bool quiet) {quiet_=quiet;}
+   void setReadTimeStamp(bool readTimeStamp){readTimeStamp_=readTimeStamp;}
+   void setDesign25(void){design25_=true;}
+   
+ private:
+    
+   std::stringstream  ss_;
+   //EventMaker::hitMapDef hitMap;
+   //int evn;
+ 
+} ;
+
+#endif
---
+++ src/tbtrackEventMaker.cpp
@@ -0,0 +1,37 @@
+/****************************************************************************
+** Author: Stefano Terzo
+**
+** Max-Planck-instiut fuer Physik (Werner-Heisenberg-Institut)
+** Foehringer Ring 6, D-80805 Muenchen, Germany
+**
+****************************************************************************/
+#include "tbtrackEventMaker.h"
+
+
+tbtrackEventMaker::tbtrackEventMaker(bool quiet, bool readTimeStamp, bool design25)
+{
+  quiet_ = quiet;
+  readTimeStamp_ = readTimeStamp;
+  design25_ = design25;
+  std::cout << "tbtrack event converter for FE-I4 (no totcode!)" << "\n";
+  evn = 0;
+}
+
+//====================================================================================
+EventMaker::hitMapDef tbtrackEventMaker::makeEvents(std::string infilename, std::string outfilename, int lv1diff, int nevt)
+{
+		       
+   int sensorID = 0;
+   EventMaker::hitDef aHit;
+   aHit.col = 0;
+   aHit.row = 0;
+   aHit.tot = 0;
+   aHit.chip= 0;
+   aHit.l1id= 0;
+   aHit.bcid= evn;//bcid;
+   if(design25_) this->design25Encode(aHit);
+   hitMap[evn][sensorID].push_back(aHit);
+  
+   return hitMap;
+}
+
