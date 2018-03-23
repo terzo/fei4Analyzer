9864c910e725c24a31483ee110fbd005579332fe
Stefano Terzo <stefano.terzo@cern.ch>
2018-03-23 14:43:45 +0100

    added CMAKE support

--- inc/tbtrackEventMaker.h
+++ inc/tbtrackEventMaker.h
@@ -42,7 +42,8 @@ class tbtrackEventMaker : public EventMaker
   ~tbtrackEventMaker(void                    ) {;}
   
    hitMapDef makeEvents(std::string infilename, std::string outfilename = "", int lv1diff = 1, int nevt = -1);
-   void setQuiet(bool quiet) {quiet_=quiet;}
+    bool writeEvents(hitMapDef &hitMap, std::string outfilename, int runNum);
+    void setQuiet(bool quiet) {quiet_=quiet;}
    void setReadTimeStamp(bool readTimeStamp){readTimeStamp_=readTimeStamp;}
    void setDesign25(void){design25_=true;}
    
